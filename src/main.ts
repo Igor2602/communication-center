21c72ef83ca38887cc656743a5a5fe2b80d9ae0c
Igor2602 <igor.vi471@gmail.com>
2026-04-23 18:23:23 -0300

    chore: setup project foundation with Vue 3, TypeScript, Pinia, PrimeVue and Sass

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { createApp } from 'vue'
+import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura'
+
+import App from './App.vue'
+import router from './router'
+import pinia from './stores'
+
+import './styles/main.scss'
+
+const app = createApp(App)
+
+app.use(pinia)
+app.use(router)
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+  },
+})
+
+app.mount('#app')
---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main class="home">
+    <h1 class="home__title">Communication Center</h1>
+  </main>
+</template>
+
+<style scoped lang="scss">
+.home {
+  @include flex-center;
+  flex: 1;
+
+  &__title {
+    font-size: $font-size-2xl;
+    font-weight: $font-weight-bold;
+    color: $color-text-primary;
+  }
+}
+</style>
